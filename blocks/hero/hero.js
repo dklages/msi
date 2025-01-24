c5f61507da63037c7ed180902ffb1a005f9a1613
dklages <dklages@gmail.com>
2025-01-24 16:59:09 -0500

    Hero updates

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,8 +1,13 @@
 export default async function decorate(block) {
   const row = block.firstElementChild;
+//  const b2 = row.querySelector('h2');
+
+  //block.append(b2);	
+	
   const bg = row.querySelector('picture');
   const bgP = bg.closest('p');
+  if (bgP) bgP.remove();	
   block.append(bg);
-  if (bgP) bgP.remove();
+
   row.classList.add('hero-body');
 }
